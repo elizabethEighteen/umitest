2cab73f4307ba3ec3104f11f2dfff60543cb5ef4
rxq <1909669885@qq.com>
2023-02-22 17:29:25 +0800

    feat: 测试图片引用tslint是否通过

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,7 +1,10 @@
+import src from '../../assets/test/1.png';
+
 const Device = () => {
   return (
     <>
       <div style={{ color: 'red' }}>2222</div>
+      <img src={src} alt="" />
     </>
   );
 };
