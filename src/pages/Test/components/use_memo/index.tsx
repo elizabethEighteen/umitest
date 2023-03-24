9cf4a8ab17a659835af00221e1d5f3f874a057bd
rxq <1909669885@qq.com>
2023-03-24 17:32:54 +0800

    feat: hooks学习

---
+++ src/pages/Test/components/use_memo/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+const genRandomName = () => {
+  console.log('in genRandomName');
+  return Math.random() + '';
+};
+
+const useName = () => {
+  // 每点一次按钮，genRandomName 就会执行一次
+  //   const [name, setName] = React.useState(genRandomName());
+  // usmemo()优化
+  const randomName = React.useMemo(() => genRandomName(), []);
+  const [name, setName] = React.useState(randomName);
+  return {
+    name,
+    setName,
+  };
+};
+
+const UseNameUI = (props: any) => {
+  console.log(props);
+  // @ts-ignore
+  // eslint-disable-line no-unused-vars
+  const { name, setName } = useName<string>('');
+  useEffect(()=>{
+    setName('222')
+  },[])
+  return (
+    <>
+      <div>{props.count}</div>
+      <div style={{ color: 'red' }}>{name}</div>
+    </>
+  );
+};
+
+export default UseNameUI;
--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,7 +1,11 @@
 import src from '../../assets/test/1.png';
 import Footer from './components/footer';
+import UseNameUI from './components/use_memo';
+import { useState } from 'react';
 
 const Device = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <div style={{ color: 'red' }}>2222</div>
@@ -9,6 +13,8 @@ const Device = () => {
       <Footer>
         <div style={{ color: 'green' }}>444</div>
       </Footer>
+      <button onClick={() => setCount((i:any) => i + 1)}>+1</button>
+      <UseNameUI count={count}></UseNameUI>
     </div>
   );
 };
